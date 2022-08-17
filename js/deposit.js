0a4d1902c61552227dd4142dc5732a2dbb902f8e
isha-150570 <isha150570@gmail.com>
2022-08-17 21:35:10 +0600

    login form added

---
+++ js/deposit.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+const depositField = document.getElementById('deposit-field');
+const depositAmount = depositField.value;
+console.log(depositAmount);
+})
+console.log('deposit')
